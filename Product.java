8acb575a9038af7b7c693ca59399974d9ecc1c55
Niceta02 <57895191+Niceta02@users.noreply.github.com>
2019-11-18 15:06:04 +0530

    Add files via upload

---
+++ Product.java
@@ -0,0 +1,78 @@
+package com.itc.products.model;
+
+public class Product {
+	private String productId;
+	private String productName;
+	private String shortDescription;
+	private String price;
+	private String productImage;
+	private int reviewRating;
+	private int reviewCount;
+	private boolean inStock;
+
+	public String getProductId() {
+		return productId;
+	}
+
+	public void setProductId(String productId) {
+		this.productId = productId;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public String getShortDescription() {
+		return shortDescription;
+	}
+
+	public void setShortDescription(String shortDescription) {
+		this.shortDescription = shortDescription;
+	}
+
+	public String getPrice() {
+		return price;
+	}
+
+	public void setPrice(String price) {
+		this.price = price;
+	}
+
+	public String getProductImage() {
+		return productImage;
+	}
+
+	public void setProductImage(String productImage) {
+		this.productImage = productImage;
+	}
+
+	public int getReviewRating() {
+		return reviewRating;
+	}
+
+	public void setReviewRating(int reviewRating) {
+		this.reviewRating = reviewRating;
+	}
+
+	public int getReviewCount() {
+		return reviewCount;
+	}
+
+	public void setReviewCount(int reviewCount) {
+		this.reviewCount = reviewCount;
+	}
+
+	public boolean isInStock() {
+		return inStock;
+	}
+
+	public void setInStock(boolean inStock) {
+		this.inStock = inStock;
+	}
+
+
+}
